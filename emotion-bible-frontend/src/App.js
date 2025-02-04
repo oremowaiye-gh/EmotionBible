edf3c6be26ee04af797efe76f9b9fcf91d131b71
Ore Mowaiye <oluwaseun.mowaiye@ten10.com>
2025-02-04 14:08:05 +0000

    Connect React frontend to backend using axios
    - Install axios for making HTTP requests
    - Create apiService.js to handle API requests
    - Update MyComponent to fetch data from the backend

---
+++ emotion-bible-frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import EmotionSearch from "./components/EmotionSearch";
+
+function App() {
+  return (
+    <div className="App">
+      <EmotionSearch />
+    </div>
+  );
+}
+
+export default App;
